87ceacc4c05c60f6211b7f9a8ccf179298224d60
milodeng <milodeng@tencent.com>
2017-04-11 09:40:19 +0800

    first commit

---
+++ QNBThread/QNBThread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QNBThread
+//
+//  Created by limodeng on 2017/4/11.
+//  Copyright © 2017年 limodeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QNBThread/QNBThread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QNBThread
+//
+//  Created by limodeng on 2017/4/11.
+//  Copyright © 2017年 limodeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
